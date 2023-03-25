9bae09a78999961110b82d54f947944b87e61a13
Kidd <kiddochang02@gmail.com>
2023-03-25 18:07:59 -0400

    snapshot

---
+++ src/pages/components/navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <>
+      <div className="flex h-24">
+        <p className="">Diversify</p>
+        login
+      </div>
+    </>
+  );
+}
---
+++ src/pages/htmlhead.tsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+export default function HtmlHead() {
+  return (
+    <Head>
+      <title>Diversify</title>
+      <meta name="description" content="placeholder" />
+      <link rel="icon" href="/favicon.ico" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossOrigin="anonymous"
+        referrerPolicy="no-referrer"
+      />
+    </Head>
+  );
+}
