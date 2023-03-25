3c945335b72e808d5cf12985da0768dfd18e6d48
Kidd <kiddochang02@gmail.com>
2023-03-25 18:44:08 -0400

    Create navbar

--- src/pages/components/navbar.tsx
+++ src/pages/components/navbar.tsx
@@ -1,9 +1,13 @@
 export default function Navbar() {
   return (
     <>
-      <div className="flex h-24">
-        <p className="">Diversify</p>
-        login
+      <div className="flex h-24 items-center justify-between text-3xl">
+        <p className="ml-16">Diversify</p>
+        <div className="mr-16">
+          <p className="rounded-full border-2 border-solid border-black px-3 py-1">
+            LOGIN
+          </p>
+        </div>
       </div>
     </>
   );
