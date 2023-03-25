3a01ca11e7b6c47d72845d74d4e11b30da2cc9b8
Bryan-Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-03-25 19:28:57 -0400

    Added carousel

--- src/pages/components/carousel.tsx
+++ src/pages/components/carousel.tsx
@@ -1,7 +1,44 @@
+// import {
+//   CarouselProvider,
+//   Slider,
+//   Slide,
+//   ButtonBack,
+//   ButtonNext,
+// } from "pure-react-carousel";
+import React from "react";
 import {
-  CarouselProvider,
-  Slider,
-  Slide,
   ButtonBack,
   ButtonNext,
+  CarouselProvider,
+  DotGroup,
+  Slide,
+  Slider,
 } from "pure-react-carousel";
+
+export default () => (
+  <CarouselProvider
+    visibleSlides={3}
+    totalSlides={8}
+    step={2}
+    naturalSlideWidth={400}
+    naturalSlideHeight={500}
+    hasMasterSpinner
+    infinite
+  >
+    <h2 className="">Infinite Carousel</h2>
+    <p>
+      A carousel that returns to the first slide if the user clicks the Next
+      button while on the last slide and returns to the last slide if the user
+      clicks the Back button while on the first slide. Also behaves similarly
+      with swiping left on the first image or right on the last image.
+    </p>
+    <Slider className="">
+      <Slide index={0}>
+        <img src="./media/img01.jpeg" />
+      </Slide>
+    </Slider>
+    <ButtonBack>Back</ButtonBack>
+    <ButtonNext>Next</ButtonNext>
+    <DotGroup />
+  </CarouselProvider>
+);
--- src/pages/components/htmlhead.tsx
+++ src/pages/components/htmlhead.tsx
@@ -6,6 +6,8 @@ const HtmlHead: React.FC = () => {
       <title>Diversify</title>
       <meta name="description" content="placeholder" />
       <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
       <link
         href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap"
         rel="stylesheet"
@@ -19,6 +21,6 @@ const HtmlHead: React.FC = () => {
       />
     </Head>
   );
-}
+};
 
 export default HtmlHead;
