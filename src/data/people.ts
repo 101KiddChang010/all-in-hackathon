eb266ab27ca6328873c9a44b5e36c7ee2a614143
Kidd <kiddochang02@gmail.com>
2023-03-26 11:48:24 -0400

    Create famous people page

---
+++ src/data/people.ts
@@ -0,0 +1,94 @@
+export const african = [
+  {
+    name: "Nelson Mandela",
+    link: "https://cdn.britannica.com/67/75567-050-4EBBE84D/Nelson-Mandela.jpg",
+  },
+  {
+    name: "Chimamanda Ngozi Adichie",
+    link: "https://media.newyorker.com/photos/5b044cc954617935a49216c8/master/w_2560%2Cc_limit/180604_r32184.jpg",
+  },
+  {
+    name: "Wole Soyinka",
+    link: "https://ca-times.brightspotcdn.com/dims4/default/3d0309a/2147483647/strip/true/crop/1334x1945+0+0/resize/1200x1750!/quality/80/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F2d%2F6a%2F0c6608cd479094fe6a1e8a2e0619%2Fla-ca-wole-soyinka.jpg",
+  },
+  {
+    name: "Miriam Makeba",
+    link: "https://i.discogs.com/ae0d-N9R-v5mx3N321j7CRfu78-sBV8EK83KyTKxhrI/rs:fit/g:sm/q:90/h:720/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTc2MDQw/LTE2MDM1NTA4Njkt/MTIyMC5qcGVn.jpeg",
+  },
+];
+
+export const indian = [
+  {
+    name: "Mahatma Gandhi",
+    link: "https://upload.wikimedia.org/wikipedia/commons/7/7a/Mahatma-Gandhi%2C_studio%2C_1931.jpg",
+  },
+  {
+    name: "Rabindranath Tagore",
+    link: "https://cdn.britannica.com/49/134949-050-242B08C7/Rabindranath-Tagore.jpg",
+  },
+  {
+    name: "Mother Teresa",
+    link: "https://m.media-amazon.com/images/M/MV5BZDk1N2MxMTQtOWI0MC00OGZmLWE5ZjQtMTZiMjMxMmM0NjdlXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg",
+  },
+  {
+    name: "Amitabh Bachchan",
+    link: "https://cdn.britannica.com/12/215912-050-02257657/Indian-actor-Amitabh-Bachchan-2013.jpg",
+  },
+];
+
+export const chinese = [
+  {
+    name: "Confucius",
+    link: "https://m.media-amazon.com/images/M/MV5BMGI3ZDAyNzAtNmFiZC00NmZhLWEyYTQtMzlkMGM0ZTMzNGZlXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg",
+  },
+  {
+    name: "Lao Tzu",
+    link: "https://images.gr-assets.com/authors/1435903703p5/2622245.jpg",
+  },
+  {
+    name: "Mao Zedong",
+    link: "https://alphahistory.com/vietnamwar/wp-content/uploads/2016/03/maozedong.jpg",
+  },
+  {
+    name: "Jackie Chan",
+    link: "https://m.media-amazon.com/images/M/MV5BMTk4MDM0MDUzM15BMl5BanBnXkFtZTcwOTI4MzU1Mw@@._V1_.jpg",
+  },
+];
+
+export const hispanic = [
+  {
+    name: "Frida Kahlo",
+    link: "https://upload.wikimedia.org/wikipedia/commons/0/06/Frida_Kahlo%2C_by_Guillermo_Kahlo.jpg",
+  },
+  {
+    name: "Pablo Picasso",
+    link: "https://mymodernmet.com/wp/wp-content/uploads/2019/02/pablo-picasso-facts.jpg",
+  },
+  {
+    name: "Che Guevara",
+    link: "https://www.history.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTk1NTgzMDIwMDEzNDYyNTg5/che-guevara-gettyimages-56854785.jpg",
+  },
+  {
+    name: "Shakira",
+    link: "https://yt3.googleusercontent.com/9z8rZjFbBf2xB2nX0A24I2uVDNSEfj-rw3hkzoLwOlgp0IqmuMuTv3yvjgAN6ufP7rpQTXxPMRs=s900-c-k-c0x00ffffff-no-rj",
+  },
+];
+
+export const native_american = [
+  {
+    name: "Sitting Bull",
+    link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk-5geltB4p1FPL1diO7ldfbw3ZmdJ_ZXlVE-T_CdNeCYvBqX2UbGATusM3FQhCFFQme903yB1X7Q&usqp=CAU&ec=48665698",
+  },
+  {
+    name: "Geronimo",
+    link: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/GeronimoRinehart.jpg/1200px-GeronimoRinehart.jpg",
+  },
+  {
+    name: "Pocahontas",
+    link: "https://hips.hearstapps.com/hmg-prod/images/pocahontas-1595---1617-wearing-traditional-attire-at-the-time-of-her-marriage-to-colonialist-john-rolfe-original-artwork-painting-by-jean-leon-gerome-ferris-photo-by-three-lionsgetty-images.jpg",
+  },
+  {
+    name: "Sacagawea",
+    link: "https://render.fineartamerica.com/images/rendered/default/print/6/8/break/images/artworkimages/medium/1/sacagawea-mark-fredrickson.jpg",
+  },
+];
