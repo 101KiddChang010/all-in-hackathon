73274743d4248343ef4f3370b264471165bcf49e
Bryan-Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-03-25 21:02:10 -0400

    WIP

--- src/pages/components/carousel.tsx
+++ src/pages/components/carousel.tsx
@@ -18,33 +18,54 @@ import Image from "next/image";
 
 const Carousel: React.FC = () => (
   <CarouselProvider
-    visibleSlides={3}
-    totalSlides={1}
+    visibleSlides={1}
+    totalSlides={2}
     step={1}
     naturalSlideWidth={250}
     naturalSlideHeight={650}
     hasMasterSpinner
-    infinite
+    // infinite
+    isPlaying
   >
-    <Slider className="h-[44.375rem] w-[78.625rem] overflow-hidden bg-red-500">
-      <Slide index={0} className="flex flex-col gap-2">
-        <p className="text-center">Chinese</p>
-        <div className="h-[43rem] w-full bg-blue-500 object-cover object-center">
-          <img
-            // className="h-auto w-[15.625rem]"
-            // h-[40.625rem] w-[15.625rem]
-            className="h-full bg-blue-500 object-cover object-center"
-            src="/images/chinese.jpeg"
+    <Slider>
+      <div
+        id="slider"
+        className="h-[44.375rem] w-[78.625rem] overflow-hidden bg-gray-200"
+      ></div>
+      <Slide index={0} className="flex h-full w-full flex-col gap-2">
+        <p className="text-center">African</p>
+        <div className="h-[43rem] w-full overflow-hidden bg-blue-500 object-cover object-center">
+          <Image
+            className="h-full w-full bg-blue-500 object-cover object-center"
+            src="/images/african.jpg"
+            alt="African American"
+            width={2000}
+            height={5200}
+            // fill
+          />
+        </div>
+      </Slide>
+
+      <Slide index={0} className="flex h-full w-full flex-col gap-2">
+        <p className="text-center">TWADSASD</p>
+        <div className="h-[43rem] w-full overflow-hidden bg-blue-500 object-cover object-center">
+          <Image
+            className="h-full w-full bg-blue-500 object-cover object-center"
+            src="/images/chinese.jpg"
             alt="Chinese American"
-            // width={250}
-            // height={650}
+            width={2000}
+            height={5200}
             // fill
           />
         </div>
       </Slide>
     </Slider>
-    <ButtonBack>Back</ButtonBack>
-    <ButtonNext>Next</ButtonNext>
+    <ButtonBack role="button" aria-label="slide backward" id="prev">
+      Back
+    </ButtonBack>
+    <ButtonNext role="button" aria-label="slide forward" id="next">
+      Next
+    </ButtonNext>
     <DotGroup />
   </CarouselProvider>
 );
