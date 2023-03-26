79f32e72ee779e14265bf1818a5fd182df849742
Kidd <kiddochang02@gmail.com>
2023-03-26 08:42:35 -0400

    Snapshot current work

--- src/pages/culture/[cultureTitle].tsx
+++ src/pages/culture/[cultureTitle].tsx
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router";
 import { type NextPage } from "next";
+import Link from "next/link";
 import HtmlHead from "../../components/htmlhead";
+import Navbar from "~/components/navbar";
 
 const CultureDetail: NextPage = () => {
   const router = useRouter();
@@ -8,7 +10,16 @@ const CultureDetail: NextPage = () => {
   return (
     <>
       <HtmlHead />
-      <h1>Details about {cultureTitle}</h1>
+      <Navbar />
+      <main className="flex flex-col items-center justify-center">
+        <h1>Learn about the {cultureTitle} ethnicity!</h1>
+        <div className="flex flex-col border border-solid border-blue-600">
+          <Link href={"../history/" + cultureTitle}>History</Link>
+          <Link href={"../culture/" + cultureTitle}>Culture</Link>
+          <Link href={"../people/" + cultureTitle}>Famous People</Link>
+          <Link href={"../gallery/" + cultureTitle}>Gallery</Link>
+        </div>
+      </main>
     </>
   );
 };
