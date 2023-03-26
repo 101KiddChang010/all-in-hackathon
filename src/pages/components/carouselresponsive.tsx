7cef9eaa92cb36b6c2189f87f52cb2ddfb819846
Bryan-Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-03-26 02:29:18 -0400

    Vastly Improved Responsiveness

---
+++ src/pages/components/carouselresponsive.tsx
@@ -0,0 +1,123 @@
+import {
+  ButtonBack,
+  ButtonNext,
+  CarouselProvider,
+  DotGroup,
+  Slide,
+  Slider,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+import Image from "next/image";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import cultures from "../data/cultures";
+import Link from "next/link";
+
+const Carousel: React.FC<{
+  className?: string;
+  visibleSlides?: number;
+  step?: number;
+}> = ({ className, visibleSlides = 1, step = 1 }) => {
+  return (
+    <CarouselProvider
+      className={className}
+      naturalSlideWidth={300}
+      naturalSlideHeight={430}
+      isIntrinsicHeight={true}
+      totalSlides={cultures.length}
+      visibleSlides={visibleSlides ? visibleSlides : 1}
+      step={step ? step : 1}
+      infinite
+    >
+      {/* <div className="relative flex items-center justify-center bg-purple-500 transition duration-200 ease-out md:bg-green-500 lg:min-w-[45vw] lg:bg-blue-500 "> */}
+      <div className="relative flex items-center justify-center transition duration-200 ease-out lg:min-w-[45vw] ">
+        {visibleSlides != cultures.length ? <ButtonBackSlide /> : null}
+        <div className="mx-auto w-full overflow-hidden  p-20 ">
+          {visibleSlides != cultures.length ? (
+            <Slider>
+              <CultureSlides />
+            </Slider>
+          ) : (
+            <CultureSlides />
+          )}
+          <DotGroup />
+        </div>
+        {visibleSlides != cultures.length ? <ButtonNextSlide /> : null}
+      </div>
+    </CarouselProvider>
+  );
+};
+
+export default Carousel;
+
+const ButtonBackSlide: React.FC = () => {
+  return (
+    <ButtonBack className="absolute left-0 z-10 cursor-pointer text-3xl text-gray-300 focus:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2">
+      <MdChevronLeft />
+    </ButtonBack>
+  );
+};
+
+const ButtonNextSlide: React.FC = () => {
+  return (
+    <ButtonNext className="absolute right-0 z-10 text-3xl text-gray-300 focus:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2">
+      <MdChevronRight />
+    </ButtonNext>
+  );
+};
+
+const CultureSlides: React.FC = () => {
+  return (
+    <div className="flex w-full items-center justify-start gap-8 transition duration-700 ease-out">
+      {/* Culture Cards */}
+      {cultures.map((culture, index) => (
+        <Slide key={index} index={index}>
+          <CultureCard
+            title={culture.title}
+            src={culture.img}
+            alt={culture.alt}
+            link={culture.link}
+            index={index}
+          />
+        </Slide>
+      ))}
+    </div>
+  );
+};
+
+const CultureCard: React.FC<{
+  classExtra?: string;
+  title: string;
+  src: string;
+  alt: string;
+  link: string;
+  index: number;
+}> = ({ classExtra, title, src, alt, index, link }) => {
+  return (
+    <Link
+      className={
+        "relative flex h-full w-full flex-1 overflow-hidden text-xl text-gray-300 transition duration-300 ease-out hover:font-bold hover:text-black " +
+        classExtra +
+        " " +
+        (index % 2 == 0 ? "flex-col-reverse" : "flex-col")
+      }
+      href={link}
+    >
+      <div
+        // className="relative h-0 overflow-hidden lg:h-[50rem]" // + "h-[60rem]"
+        // className="relative h-[50vh] w-[25vw] overflow-hidden"
+        className="relative h-[50vh] w-full overflow-hidden lg:w-[10vw]"
+      >
+        {/* Ratio for cards is 5:13 */}
+        <Image
+          className="h-full w-full object-contain object-center transition duration-[225ms] ease-in md:object-cover md:hover:scale-105"
+          fill
+          src={src}
+          alt={alt}
+        />
+      </div>
+      <div className="relative w-full p-4 text-center capitalize">
+        <h2>{title}</h2>
+      </div>
+    </Link>
+  );
+};
--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,37 +1,46 @@
 import { type NextPage } from "next";
 import Navbar from "./components/navbar";
 import HtmlHead from "./components/htmlhead";
-import Carousel from "./components/carousel";
+// import Carousel from "./components/carousel";
+import Carousel from "./components/carouselresponsive";
 
 const Explore: NextPage = () => {
   return (
     <>
       <HtmlHead />
-      <div className="h-screen overflow-hidden">
+      <div className="flex h-screen flex-col gap-4 overflow-hidden">
         <Navbar />
         {/* <Navbar classNameExtras="absolute" /> */}
         <main className="animate-fade-in animate-duration-1 flex h-full w-full flex-col items-center justify-center align-middle">
-          {/* <Carousel /> */}
+          <section className="flex w-full overflow-hidden px-10 pb-16 sm:px-16 md:px-10 lg:px-8">
+            {/* <Carousel
+              // className="hidden max-w-screen-xl bg-red-500 xl:block"
+              className="flex w-full flex-1 items-center justify-center bg-red-500 align-middle "
+              visibleSlides={5}
+              step={5}
+            />
+          </section> */}
 
-          {/* Extra Large Screens */}
-          <Carousel
-            classExtra="hidden max-w-screen-xl xl:block"
-            visibleSlides={5}
-            step={5}
-          />
-          {/* Large Screens */}
-          <Carousel
-            classExtra="hidden max-w-screen-xl max-h-[30rem] lg:block xl:hidden bg-red-500 max-w-screen-xl"
-            visibleSlides={3}
-            step={3}
-          />
-          {/* Medium Screens */}
-          <Carousel
-            classExtra="hidden md:block lg:hidden bg-green-500 max-w-screen-md"
-            visibleSlides={2}
-          />
-          {/* Small Screens */}
-          <Carousel classExtra="block md:hidden max-w-screen-sm" />
+            {/* Extra Large Screens */}
+            <Carousel
+              className="hidden w-full flex-1 items-center justify-center align-middle xl:flex"
+              visibleSlides={5}
+              step={5}
+            />
+            {/* Large Screens */}
+            <Carousel
+              className="hidden w-full flex-1 items-center justify-center align-middle lg:flex xl:hidden"
+              visibleSlides={3}
+              step={3}
+            />
+            {/* Medium Screens */}
+            <Carousel
+              className=" hidden w-full flex-1 items-center justify-center align-middle md:flex lg:hidden"
+              visibleSlides={2}
+            />
+            {/* Small Screens */}
+            <Carousel className="flex w-full max-w-screen-sm flex-1 items-center justify-center align-middle  md:hidden" />
+          </section>
         </main>
       </div>
     </>
